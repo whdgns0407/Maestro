bb392764f47632365d48d7271f926fe0851e5f0a
whdgns0407 <whdgns0407@gmail.com>
2022-10-27 21:55:02 +0900

    세션 삭제 session_destroy 로그아웃

---
+++ logout.php
@@ -0,0 +1,19 @@
+﻿<html>
+	<head>
+	</head>
+	<body>
+		<?php
+		session_start();        
+		if(isset($_SESSION['email'])){
+			echo "로그아웃 중입니다.";
+			unset($_SESSION['email']);
+			session_destroy();
+			echo "<script> window.top.location.reload(); </script>";
+			echo "<meta http-equiv=refresh content='0 url=./index.php'>";
+			
+			exit();
+		}
+			echo "<meta http-equiv=refresh content='0 url=./index.php'>";
+		?>
+	</body>
+<html>
