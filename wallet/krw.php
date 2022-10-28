d3af7da27bcc5d198a82be3e2a74727fbb705895
whdgns0407 <whdgns0407@gmail.com>
2022-10-29 04:17:44 +0900

    원화

---
+++ wallet/krw.php
@@ -0,0 +1,56 @@
+﻿<?php
+	session_start();
+	if(isset($_SESSION['email']))
+	{
+		include("../config.php");
+		$query = mysql_query("select * from accounts WHERE email='{$_SESSION['email']}'");
+		$queryinformation = mysql_fetch_array($query);
+		$walletquery = mysql_query("select * from wallet WHERE id='{$queryinformation['id']}'");
+		$walletinformation = mysql_fetch_array($walletquery);
+	?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>마에스트로거래소 - 지갑관리 - 원화</title>
+		<style>
+			nav{
+				width:100%;
+			}
+		
+			content{
+				width:100%;
+			}
+		
+			a{
+				font-size:30px;
+				margin-right:40px;
+				text-decoration:none;
+				color:black;
+				font-weight:bold;
+			}
+			a:hover{
+				font-size:35px;
+			}
+		</style>
+	</head>
+	<body>
+		<nav>
+			<font style="text-align: center;">
+			<img width="40px" height="40px" src="krw.png" alt="원화">
+			<a href="./deposit/krw.php" target="cont_a">  충전</a>
+			<a href="./withdraw/krw.php" target="cont_a">출금</a>
+			<a href="./history/krw_inout.php" target="cont_a">내역</a>
+			<iframe name="cont_a" width="100%" height="850px" src="./deposit/krw.php"></iframe>
+			</font>
+		</nav>
+	</body>
+</html>
+<?php	
+	}
+	else 
+	{
+		echo "<script>alert('로그인 후 시도하여 주세요.')</script>";
+		echo "<meta http-equiv=refresh content='0 url=../../../login.php'>";
+		exit();
+	}
+?>
