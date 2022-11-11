931c14cffecc48e0a70fadf8a104446ae9aeaadd
whdgns0407 <whdgns0407@gmail.com>
2022-11-12 01:45:35 +0900

    메인페이지

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width", initial-scale="1">
-		<title>마에스트로거래소 - 컴퓨터 과학과</title>
+		<title>마에스트로거래소 - 메인페이지</title>
 		<link rel="stylesheet" href="./css/bootstrap.css">
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script src="js/bootstrap.js"></script>
